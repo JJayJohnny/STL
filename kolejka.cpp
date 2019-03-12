7386853f79a5b8c6b1042a9ce46003b9e7f64aba
JJayJohnny <43695474+JJayJohnny@users.noreply.github.com>
2019-03-12 12:31:47 +0100

    intitial commit

---
+++ kolejka.cpp
@@ -0,0 +1,40 @@
+﻿// kolejka.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	char sign;
+	queue<int> q;
+
+	while (cin >> sign)
+	{
+		if (sign == '+')
+		{
+			cin >> number;
+			q.push(number);
+		}
+		else if (sign == '-')
+		{
+			if (!q.empty())
+				q.pop();
+			else cout << "ERROR" << endl;
+		}
+		else cout << "WHATTTTT?" << endl;
+	}
+
+	while (!q.empty())
+	{
+		cout << q.front() << " ";
+		q.pop();
+	}
+
+	return 0;
+}
+
